366927f0e80eb2375c3fa00bb31b4fdd67ac6611
FreeCoche Developer <lead-generator@freecoche.com>
2026-02-21 18:50:09 +0000

    fix: ensure scroll to top on page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import LegalNotice from './pages/LegalNotice';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/privacidad" element={<PrivacyPolicy />} />
